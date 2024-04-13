b62058d471aeeb4fb71d910ae5e67bb91527df61
LuisPerezLec <luigiperez2002@gmail.com>
2024-04-12 21:27:00 -0600

    Complete ORM

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ var connString = builder.Configuration.GetConnectionString("MySqlConnection");
 builder.Services.AddDbContext<GameStoreContext>( options =>
     options.UseMySql(connString,
     new MySqlServerVersion(new Version(8, 0, 29)))
-); 
+);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+app.MigrateDb();
 
 app.Run();
